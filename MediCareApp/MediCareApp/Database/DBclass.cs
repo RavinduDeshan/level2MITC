59ccc44e6596e117e77de21d82d40a180cac269c
P D R Deshan <ravindudeshan9865@gmail.com>
2020-09-12 15:39:51 +0530

    db connection

---
+++ MediCareApp/MediCareApp/Database/DBclass.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediCareApp.Database
+{
+    class DBclass
+    {
+
+        private String conString = @"SERVER=abcdatabase.mysql.database.azure.com;PORT=3306;DATABASE=mydb;UID=abcadmin@abcdatabase;PASSWORD=ABC@123abc";
+        private MySql.Data.MySqlClient.MySqlConnection con;
+
+        public MySqlConnection getConnection()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(conString);
+                this.con = con;
+                this.con.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Data Connection Error Occured");
+            }
+            return this.con;
+
+        }
+        public void closeConnection()
+        {
+
+            this.con.Close();
+
+
+        }
+    }
+}
